251ab7a7af94ccbc6aef00c6fc02b50dbc1bb673
MsD <mahenmsd@gmail.com>
2020-02-14 10:48:01 +0530

    change test request

--- src/main/test/com/msd/api/controller/TestController.java
+++ src/main/test/com/msd/api/controller/TestController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin()
 @RestController
-@RequestMapping({ "/employees" })
+@RequestMapping({ "/persons" })
 public class TestController {
 
 	private List<Person> personList = createList();
